286a8eb06fc47601280e9a68655c077de9a6553c
nanda77 <71319641+nanda77@users.noreply.github.com>
2021-08-03 23:50:03 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -236,9 +236,9 @@ function setPanaroma(imageSection, imgPath) {
 
 
 function getToken() {
-        surl = "http://www.share.buildworks.in/B5C73E2A"; //window.location.href;
+        surl = window.location.href; //window.location.href;
         var id =  surl.substring(surl.lastIndexOf('/') + 1);
-        //alert (surl);
+        alert (surl);
         return id;
         
      
